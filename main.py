7e3a7e22ff03cede15f1a9aee40b7fd5feb698f0
zenzx <2362315840@qq.com>
2021-06-17 18:34:15 +0800

    main文件检查参数

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import sys
 import sendEmail
 
 if __name__ == '__main__':
+	with open('./content.html', encoding='utf-8', mode='r') as f:
+		contents = f.read()
 	if sys.argv[1] == 'gmail':
-		sendEmail.sender_mail("<h1>FwRot</h1>")
+		sendEmail.sender_mail("<h1>FwRebot</h1>")
+	elif sys.argv[1] == '163' or sys.argv[1] == 'qq':
+		sendEmail.sender_mail("<h1>FwRebot</h1>")
+	else:
+		print("请选择你要使用的邮箱系统，目前支持QQ邮箱，网易邮箱，谷歌邮箱。")
+		print("e.g. ..> python main gmail/qq/163")
