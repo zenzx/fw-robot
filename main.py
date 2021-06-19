a1f226a4e360a84309924a2299af782165c28f8d
zenzx <2362315840@qq.com>
2021-06-19 12:43:14 +0800

    修改HTML文件名

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sendEmail
 
 if __name__ == '__main__':
-	with open('./content.html', encoding='utf-8', mode='r') as f:
+	with open('./goodMorning.html', encoding='utf-8', mode='r') as f:
 		contents = f.read()
 	sendEmail.sender_mail(contents)
