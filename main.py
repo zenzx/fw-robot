d0d5cb9500eaf41f094c367ce637b1780952e42e
zenzx <2362315840@qq.com>
2021-06-29 21:40:45 +0800

    引入模板

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sendEmail
 
 if __name__ == '__main__':
-	with open('./html/goodMorning.html', encoding='utf-8', mode='r') as f:
+	with open('./html/Outdoors.html', encoding='utf-8', mode='r') as f:
 		contents = f.read()
 	sendEmail.sender_mail(contents)
